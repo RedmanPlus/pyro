b10bc020d093675b18ce9961b648a95a0a4e8b01
Antony Redman <RumataYounger@gmail.com>
2024-03-08 08:43:19 +0300

    add tokenizer support for the comma

--- src/tokens.py
+++ src/tokens.py
@@ -1,4 +1,3 @@
-import re
 from enum import Enum, auto
 from typing import Optional
 
@@ -11,11 +10,18 @@ class TokenType(Enum):
     MINUS = auto()
     MUL = auto()
     DIV = auto()
+    COMMA = auto()
     NEWLINE = auto()
 
 
 class Token:
-    def __init__(self, token_type: TokenType, line: int, pos: int, content: Optional[str] = None):
+    def __init__(
+        self,
+        token_type: TokenType,
+        line: int | None = None,
+        pos: int | None = None,
+        content: Optional[str] = None,
+    ):
         self.token_type = token_type
         self.line = line
         self.pos = pos
@@ -26,48 +32,96 @@ class Token:
 
 
 class Tokenizer:
-    TOKEN_MAP = {
-        r"^[a-zA-Z_$][\w$]*$": TokenType.IDENT,
-        r"^[0-9]*$": TokenType.NUMBER,
-        r"=": TokenType.EQ,
-        r"\+": TokenType.PLUS,
-        r"\-": TokenType.MINUS,
-        r"\*": TokenType.MUL,
-        r"\/": TokenType.DIV,
-    }
-
     def __init__(self, code: str):
-        code_lines: list[str] = code.split("\n")
+        self.code = code
         self.tokens: list[Token] = []
-        for i, line in enumerate(code_lines):
-            line_elems = line.split(" ")
-            for elem in line_elems:
-                if elem == " " or elem == "":
-                    continue
-                elem = elem.strip()
-                token_type = self._match_token(elem)
-                pos = line.find(elem) + 1
-                token = Token(
-                    token_type=token_type,
-                    line=i + 1,
-                    pos=pos,
-                    content=elem if token_type in [TokenType.IDENT, TokenType.NUMBER] else None,
-                )
-                self.tokens.append(token)
-            self.tokens.append(
-                Token(token_type=TokenType.NEWLINE, line=i + 1, pos=len(line), content=None)
-            )
+        self._process_code()
 
-    def _match_token(self, elem: str) -> TokenType:
-        for rejex in self.token_rejexes:
-            match = re.match(rejex, elem)
-            if match:
-                return self.TOKEN_MAP[rejex]
+    def _process_code(self):
+        while self._peek(0) is not None:
+            self._trim_whitespace()
+            current_char: str | None = self._peek(0)
+            if current_char is None:
+                break
+            if current_char.isalpha():
+                self._process_alnum()
+            if current_char.isdigit():
+                self._process_digit()
+            if current_char == "=":
+                self._process_eq()
+            if current_char == "+":
+                self._process_plus()
+            if current_char == "-":
+                self._process_minus()
+            if current_char == "*":
+                self._process_mul()
+            if current_char == "/":
+                self._process_div()
+            if current_char == ",":
+                self._process_comma()
+        self.tokens.append(Token(token_type=TokenType.NEWLINE))
 
-        raise Exception(
-            f"Token {elem} is not known to the tokenizer, choices are: {self.token_rejexes}"
+    def _process_alnum(self):
+        char_buff: list[str] = []
+        while self._peek(0) is not None and self._peek(0).isalnum():
+            current_char = self._consume()
+            char_buff.append(current_char)
+        value = "".join(char_buff)
+        token = Token(
+            token_type=TokenType.IDENT,
+            content=value,
         )
+        self.tokens.append(token)
 
-    @property
-    def token_rejexes(self) -> list[str]:
-        return list(self.TOKEN_MAP.keys())
+    def _process_digit(self):
+        char_buff: list[str] = []
+        while self._peek(0) is not None and self._peek(0).isdigit():
+            current_char = self._consume()
+            char_buff.append(current_char)
+
+        if self._peek(0) is not None and self._peek(0).isalpha():
+            raise Exception(f"Variables cannot start with digits, given - {''.join(char_buff)}")
+
+        value = "".join(char_buff)
+        token = Token(token_type=TokenType.NUMBER, content=value)
+        self.tokens.append(token)
+
+    def _process_eq(self):
+        self._consume()
+        self.tokens.append(Token(token_type=TokenType.EQ))
+
+    def _process_plus(self):
+        self._consume()
+        self.tokens.append(Token(token_type=TokenType.PLUS))
+
+    def _process_minus(self):
+        self._consume()
+        self.tokens.append(Token(token_type=TokenType.MINUS))
+
+    def _process_mul(self):
+        self._consume()
+        self.tokens.append(Token(token_type=TokenType.MUL))
+
+    def _process_div(self):
+        self._consume()
+        self.tokens.append(Token(token_type=TokenType.DIV))
+
+    def _process_comma(self):
+        self._consume()
+        self.tokens.append(Token(token_type=TokenType.COMMA))
+
+    def _peek(self, position: int) -> str | None:
+        if self.code:
+            return self.code[position]
+        return None
+
+    def _consume(self) -> str:
+        result = self.code[0]
+        self.code = self.code[1:]
+        return result
+
+    def _trim_whitespace(self):
+        while self._peek(0) is not None and self._peek(0).isspace():
+            if self._peek(0) == "\n":
+                self.tokens.append(Token(token_type=TokenType.NEWLINE))
+            self._consume()
