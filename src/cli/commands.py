f1008deeecfd94ba0e5e19ff5ef19cf8605ef66d
Antony Redman <RumataYounger@gmail.com>
2024-04-01 13:44:21 +0300

    add local changes for compilation in debug mode

--- src/cli/commands.py
+++ src/cli/commands.py
@@ -13,7 +13,9 @@ def handle_output_file(dst: str, asm: str, debug: bool = False) -> None:
     subprocess.run(["nasm", "-felf64", f"{dst}.asm"])
     if debug:
         subprocess.run(
-            f"ld -o {dst} {dst}.o -dynamic-linker /lib64/ld-linux-x86-64.so.2 -L./raylib/ -lc".split()
+            f"ld -o {dst} {dst}.o -dynamic-linker /lib64/ld-linux-x86-64.so.2 -L./raylib/ -lc".split(),
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
         )
     else:
         subprocess.run(["ld", "-o", f"{dst}", f"{dst}.o"])
